14ef68e13d28ef88f424ab2106ba7480192cf226
lucenasoft <getugbr@gmail.com>
2023-01-23 08:33:06 -0300

    feat: add connections and send message

--- src/main/java/com/lucenasoft/wschatjava/handler/ChatWebSocketHandler.java
+++ src/main/java/com/lucenasoft/wschatjava/handler/ChatWebSocketHandler.java
@@ -1,7 +1,32 @@
 package com.lucenasoft.wschatjava.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private final List<WebSocketSession> webSocketSession = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        webSocketSession.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : webSocketSession) {
+            webSocketSession.sendMessage(message);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        webSocketSession.remove(session);
+    }
 
 }
